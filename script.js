4471991e4ec04e12d01bd2612a5c4ac02e0d829e
Treecher <martin.vrobel308@gmail.com>
2025-03-16 21:01:30 +0100

    added the random changing letters as effect which looks cool

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-for (let i = 1; i < 65; i++){
-    if (i % 2 == Math.ceil(Math.random() * 2)-1){
-        document.getElementById(i).textContent = "a";
+function random(){
+    reset()
+    for (let i = 1; i < 65; i++){
+        if (i % 2 == Math.ceil(Math.random() * 2)-1){
+            document.getElementById(i).textContent = String.fromCharCode(Math.floor(Math.random() * (122 - 65 + 1)) + 65);
+        }
     }
 }
+
+function reset(){
+    for (let i = 1; i < 65; i++){
+        document.getElementById(i).textContent = "";
+    }
+}
+
+setInterval(random, 1000)
