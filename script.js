1fa25f563e26b0033ce05533c021ccedff640542
Treecher <martin.vrobel308@gmail.com>
2025-03-16 21:24:46 +0100

    made template to complete moves etc

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-function random(){
+var board = [
+    ["white rook","white knight","white bishop","white queen","white king","white bishop","white knight","white rook"],
+    ["white pawn","white pawn","white pawn","white pawn","white pawn","white pawn","white pawn","white pawn"],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["black pawn","black pawn","black pawn","black pawn","black pawn","black pawn","black pawn","black pawn"],
+    ["black rook","black knight","black bishop","black queen","black king","black bishop","black knight","black rook"]
+]
+
+var whitePieces = [
+    "white pawn", "white rook",
+    "white knight", "white bishop",
+    "white queen", "white king",
+]
+
+var blackPieces = [
+    "black pawn", "black rook",
+    "black knight", "black bishop",
+    "black queen", "black king",
+]
+
+function boardDraw(){
     reset()
-    for (let i = 1; i < 65; i++){
-        if (i % 2 == Math.ceil(Math.random() * 2)-1){
-            document.getElementById(i).textContent = String.fromCharCode(Math.floor(Math.random() * (122 - 65 + 1)) + 65);
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            let ID = i * 8 + j + 1; 
+            document.getElementById(ID).textContent = board[i][j]
+            document.getElementById(ID).style.color = checkTeam(board[i][j])
+        }
+    }
+}
+
+function checkTeam(piece){
+    for (var item of whitePieces) {
+        if (piece == item){
+            return "red"
+        }
+    }
+
+    for (var item of blackPieces) {
+        if (piece == item){
+            return "blue"
         }
     }
 }
@@ -13,4 +52,4 @@ function reset(){
     }
 }
 
-setInterval(random, 1000)
+boardDraw()
